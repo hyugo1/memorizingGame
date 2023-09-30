2be40e7fe1751254e5815c92d68dd973d2921b95
t62839hs <hyugo.senzai@student.manchester.ac.uk>
2023-09-30 20:38:41 +0100

    made the content images instead of emojis

--- memorizeWebsite.js
+++ memorizeWebsite.js
@@ -1,8 +1,24 @@
-const cardEmoji = ['😍', '😍', '😁', '😁', '🧐', '🧐', '😎', '😎', '🥰', '🥰', '😈', '😈', '🤩', '🤩', '🥶', '🥶'];
-const shuffle_emoji = cardEmoji.sort(() => (Math.random() > 0.5) ? 2 : -1);
+const cardImages = [
+    '8ball.png',
+    'american-football.png',
+    'baseball.png',
+    'basketball.png',
+    'football.png',
+    'golfball.png',
+    'tennisball.png',
+    'volleyball.png'
+];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffleArray(cardImages);
+
 const memoryGame = document.querySelector('.memory-game');
-
-
 
 let flippedCards = [];
 let matches = 0;
@@ -10,41 +26,42 @@ let currentPlayer = 1;
 const player1ScoreElement = document.querySelector('#player1 .score');
 const player2ScoreElement = document.querySelector('#player2 .score');
 
-
-
 function switchPlayer() {
     currentPlayer = currentPlayer === 1 ? 2 : 1;
-    highlightCurrentPlayer(); 
+    highlightTheCurrentPlayer(); 
 }
 
-for (let i = 0; i < shuffle_emoji.length; i++) {
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `<div class="cardInner">
-                          <!-- <div class="cardFront">♠️</div> --!>
-                          <div class="cardFront"><img src="card.png" alt="Cards"></img></div>
-                          <div class="cardBack">${shuffle_emoji[i]}</div>
-                      </div>`;
-
-    card.addEventListener('click', function () {
-        if (!this.classList.contains('cardOpen') && flippedCards.length < 2) {
-            this.classList.add('cardOpen');
-            flippedCards.push(this);
-
-            if (flippedCards.length === 2) {
-                setTimeout(checkForMatch, 500);
+for (let i = 0; i < cardImages.length; i++) {
+    for (let j = 0; j < 2; j++) { 
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `<div class="cardInner">
+                                <div class="cardFront"><img src="card.png" alt="Cards"></img></div>
+                                <div class="cardBack"><img src="${cardImages[i]}" alt="Content Image"></div>
+                          </div>`;
+    
+        card.addEventListener('click', function () {
+            if (!this.classList.contains('cardOpen') && flippedCards.length < 2) {
+                this.classList.add('cardOpen');
+                flippedCards.push(this);
+    
+                if (flippedCards.length === 2) {
+                    setTimeout(checkForMatch, 500);
+                }
             }
-        }
-    });
-
-    memoryGame.appendChild(card);
+        });
+    
+        memoryGame.appendChild(card);
+    }
 }
 
 
 function checkForMatch() {
     const [card1, card2] = flippedCards;
+    const img1 = card1.querySelector('.cardBack img');
+    const img2 = card2.querySelector('.cardBack img');
 
-    if (card1.querySelector('.cardBack').textContent === card2.querySelector('.cardBack').textContent) {
+    if (img1.src === img2.src) {
         card1.classList.add('cardMatch');
         card2.classList.add('cardMatch');
         card1.classList.remove('cardOpen');
@@ -52,14 +69,14 @@ function checkForMatch() {
         flippedCards = [];
         matches++;
 
-        // Update player if they scores/match
+        // Update player scores when you match
         if (currentPlayer === 1) {
             player1ScoreElement.textContent = parseInt(player1ScoreElement.textContent) + 1;
         } else {
             player2ScoreElement.textContent = parseInt(player2ScoreElement.textContent) + 1;
         }
 
-        if (matches === cardEmoji.length / 2) {
+        if (matches === cardImages.length / 2) {
             alert('Congratulations! Game Over!');
         }
     } else {
@@ -73,7 +90,7 @@ function checkForMatch() {
 }
 
 
-function highlightCurrentPlayer() {
+function highlightTheCurrentPlayer() {
     const player1 = document.getElementById('player1');
     const player2 = document.getElementById('player2');
     
@@ -86,4 +103,4 @@ function highlightCurrentPlayer() {
     }
 }
 
-highlightCurrentPlayer();
+highlightTheCurrentPlayer();
